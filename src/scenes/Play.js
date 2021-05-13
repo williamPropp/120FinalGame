9ef6a1ecaa1ca4c730fe220f0ac1b12f2b5f52db
jgpicos <jpicos@ucsc.edu>
2021-05-12 19:50:11 -0700

    Conveyor and dragable bin

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -27,7 +27,21 @@ class Play extends Phaser.Scene {
         // this.soundtrack.play();
         this.bg = this.add.rectangle(0, 0, game.config.width, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 
-        this.button = this.add.circle(game.config.width/2, game.config.height/2, game.config.width/8, 0xFF2255).setOrigin(0.5,0.5);
+        this.conveyor = this.add.rectangle(0, 450, game.config.width - 250, game.config.height/10, 0x000000).setOrigin(0 ,0);
+
+        this.bin = this.add.rectangle(25, 600, 100, 200, 0x808080).setOrigin(0 ,0);
+        this.bin.setInteractive();
+        this.input.setDraggable(this.bin);
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+        });
+        this.input.on('dragend', function(pointer, gameObject, dragX, dragY, dropped) {
+            gameObject.x = 25;
+            gameObject.y = 600;
+        });
+
+        this.button = this.add.circle(game.config.width - 200, game.config.height/4, game.config.width/8, 0xFF2255).setOrigin(0.5,0.5);
         this.button.setInteractive({
             draggable: false,
             useHandCursor: false
